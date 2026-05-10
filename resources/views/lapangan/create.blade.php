502e46c5d11be3ba11e774e7aca2dba3dca15efb
unknown <khusnul.kh783@gmail.com>
2026-05-10 00:49:49 -0700

    beresin api

--- resources/views/lapangan/create.blade.php
+++ resources/views/lapangan/create.blade.php
@@ -14,7 +14,7 @@
                         @csrf
 
                         <div class="mb-4">
-                            <label class="block text-gray-700 text-sm font-bold mb-2">Nama Lapangan</label>
+                            <label class="block text-gray-700 text-sm font-bold mb-2">Nama Lapangann</label>
                             <input type="text" name="nama_lapangan" required placeholder="Contoh: Lapangan VIP A"
                                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
                         </div>
