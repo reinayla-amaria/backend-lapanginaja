09cc0bfc2a789bf1fbb1919a8dade5e57bb0971e
unknown <khusnul.kh783@gmail.com>
2026-05-21 10:19:37 -0700

    dan yap

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login - LapanginAja</title>
+    <title>Login - LapanginAjaAa</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
         rel="stylesheet">
