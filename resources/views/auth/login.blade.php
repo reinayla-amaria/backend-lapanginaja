14a6f26b8961b2b7011c676902ebd5f148dc9461
unknown <khusnul.kh783@gmail.com>
2026-05-21 10:27:09 -0700

    dan yap

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login - LapanginAjaAa</title>
+    <title>Login - LapanginAja</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
         rel="stylesheet">
@@ -33,7 +33,7 @@
                 class="w-56 h-auto object-contain mt-10 drop-shadow-xl hover:scale-105 transition-transform duration-300">
 
             <p class="mt-12 text-center text-gray-500 font-medium px-8 text-sm max-w-sm leading-relaxed">
-                Kelola LAPANGAN badminton Anda dan pantau transaksi dengan mudah dalam satu aplikasi digital.
+                Kelola lapangan badminton Anda dan pantau transaksi dengan mudah dalam satu aplikasi digital.
             </p>
 
             <div class="absolute -top-2 -bottom-2 left-full w-24 hidden md:block pointer-events-none -ml-2 text-white">
