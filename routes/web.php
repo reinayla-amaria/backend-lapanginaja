52fd963009d96e204713713c49553035be4fbb74
reinayla_amaria <reinaylamaria@gmai.com>
2026-06-08 22:16:33 +0700

    tampilan laravel

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Models\Lapangan;
 use App\Models\Booking;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 Route::get('/jalanin-migrasi', function () {
@@ -64,7 +64,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 
-   
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
